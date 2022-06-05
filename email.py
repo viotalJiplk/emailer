7ed387d5640c2cbc27b2b1d2c268d643860fb8c0
Vojtech Varecha <viotal.varecha@outlook.com>
2022-06-05 20:40:03 +0200

    osloveni :D

--- email.py
+++ email.py
@@ -17,12 +17,12 @@ def login(s, email, password):
     s.ehlo()
     s.login(email, password)
 
-def createmsg(subject, sender, parts):
+def createmsg(subject, sender, parts, osloveni):
     msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
     msg['From'] = sender
     for x in parts:
-        msg.attach(MIMEText(load_file(x["file"]),x["mime"]))
+        msg.attach(MIMEText(load_file(x["file"]).format(osloveni=osloveni),x["mime"]))
     return msg
 
 def load_file(filename):
@@ -31,18 +31,15 @@ def load_file(filename):
     fp.close()
     return text
 
-msg = createmsg("test", config["sender"], config["emailparts"])
-emails = []
+s = smtplib.SMTP(host=config["mailserver"], port=config["mailserverport"], timeout=4.4)
+
+login(s, config["sender"], config["password"])
 
 with open(str(pathlib.Path(__file__).parent.resolve())+"/"+config["csv"], newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
-        emails.append(row[0])
+        msg = createmsg("test", config["sender"], config["emailparts"], row[1])
+        msg["to"] = row[0]
+        s.send_message(msg)
 
-s = smtplib.SMTP(host=config["mailserver"], port=config["mailserverport"], timeout=4.4)
-
-login(s, config["sender"], config["password"])
-for email in emails:
-    msg["to"] = email 
-    s.send_message(msg)
 s.quit()
